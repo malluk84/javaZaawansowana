a4f52cfb6727dfa7f251ac68c7e8b4d3a43038fa
chodekm <chodekm@gmail.com>
2021-10-23 12:13:44 +0200

    Programowanie funkcyjne

--- src/main/java/funkcyjne/ReferncjeMetod.java
+++ src/main/java/funkcyjne/ReferncjeMetod.java
@@ -8,10 +8,19 @@ public class ReferncjeMetod {
 
         Consumer<String> consumer = valueToAccept -> System.out.println(valueToAccept);
 
-        System.out.println();
-        Consumer<String> consume2 = System.out::println;
+        /*
+        Aby uzyc referencji do metody, lambda musi otrzymywac jeden argument i:
+        a) argument wejściowy lambdy jest argumentem metody (do ktorej robimy referencje)
+        lub
+        b) na argumencie wejściowym wywoływana jest bezargumentowa metoda
+         */
 
-        Consumer<String> consume3 = String::length;
+        // przypadek a)
+        System.out.println(); // wywolanie metody
+        Consumer<String> consume2 = System.out::println; // referencja do metody
+
+        // przypadek b)
+        Consumer<String> consume3 = String::length; // referncja do metody
         ;
 
         consume2.accept("Ala ma kota");
